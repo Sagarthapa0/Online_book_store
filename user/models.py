ed5c3bf7b12471cf889d0b85fc9b4b54218109c8
Sagar Thapa <st163528@gmail.com>
2024-05-07 15:37:04 +0545

    first commit

---
+++ user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    updated_at=models.DateTimeField(auto_now=True)
+    seller=models.BooleanField(default=False)
+    #buyer=models.BooleanField(default=False)
+    
+
+    def __str__(self) -> str:
+        return self.username
---
+++ user/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import views
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+
+
+# Create your views here.
+from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveAPIView,DestroyAPIView,UpdateAPIView
+from .serializers import UserCreateSerializer,UserListSerializer,UserLoginSerializer
+from .models import User
+
+
+def get_token_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return{
+        "refresh":str(refresh),
+        "access":str(refresh.access_token),
+    }
+
+class UserCreateView(CreateAPIView):
+    serializer_class =UserCreateSerializer
+
+    def post(self,request,*args,**kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user=serializer.save()
+
+        token =  get_token_for_user(user)
+
+        return Response({'token':token, 'msg':'Signup Successfull'},status=status.HTTP_201_CREATED)
+
+        
+
+
+class UserLoginView(TokenObtainPairView):
+    serializer_class=UserLoginSerializer
+    def post(self,request):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "refresh":str(refresh),
+            "access":str(refresh.access_token),
+        },status=status.HTTP_200_OK)
+
+
+class LogoutView(APIView):
+    def post(self,request):
+        refresh_token =  request.data.get('refresh_token')
+        if not refresh_token:
+            return Response({'error':'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            token=RefreshToken(refresh_token)
+            token.blacklist()
+        
+        except Exception as e:
+            return Response({'error':'Invalid refresh'},status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({'success':'Successfully logged out'}, status=status.HTTP_200_OK)
+    
+
+
+
+class UserListView(ListAPIView):
+    queryset =User.objects.all()
+    serializer_class=UserListSerializer
+
+
+class SellerListView(ListAPIView):
+    queryset =User.objects.all()
+    serializer_class=UserListSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(seller=True)
+
+
+class BuyerListView(ListAPIView):
+    queryset =User.objects.all()
+    serializer_class=UserListSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(buyer=True)
+    
+
+class UserRetrieveView(RetrieveAPIView):
+    queryset =User.objects.all()
+    serializer_class=UserListSerializer
+    
+
+class UserUpdateView(UpdateAPIView):
+    queryset =User.objects.all()
+    serializer_class=UserListSerializer
+
+    
+class UserDeleteView(DestroyAPIView):
+    queryset =User.objects.all()
+    serializer_class=UserListSerializer
+    
